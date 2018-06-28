2034b0421ae390133348ed541b05610f9f010ad7
ZakharPustovgarov <deadspace1999@mail.ru>
2018-06-28 23:10:55 +0400

    Добавил сайт

---
+++ scripts/comments.js
@@ -0,0 +1,31 @@
+Vue.component('comment', {
+    props:['inner'],
+    template:`<div class="smbdcomment">
+                <div class="person">
+                    <h3>{{inner.user}}</h3>
+                    <img src="../assets/anonymus.png" alt="anonymus">
+                </div>
+            
+                <div class="comment">
+                    <p>{{inner.comment}}</p>
+                </div>
+            </div>`
+    })
+
+    new Vue({
+        el:'#comments',
+        data:{comments:[],
+            nextCommentId:0
+        },
+        methods: {
+            addComment: function () {
+                this.comments.push({
+                    id: this.nextCommentId++,
+                    user: this.user,
+                    comment: this.comment
+                })
+                this.user = '',
+                this.comment = ''
+            }
+        }
+    })
